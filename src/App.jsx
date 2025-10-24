dec2eea915368bb77f0ebaa909f74aad9144f2c3
grace dong <gracezilun.dong@gmail.com>
2025-10-23 17:07:08 -0700

    Update welcome message

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        CHANGE TEST! Click on the Vite and React logos to learn more
       </p>
     </>
   )
